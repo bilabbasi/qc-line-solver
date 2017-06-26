e598df70dc42ad108859b2a2703250961a70b34b
Bilal Abbasi <bilal.abbasi.ba@gmail.com>
2017-06-26 19:33:43 -0400

    v2

--- ndim_linesolver.m
+++ ndim_linesolver.m
@@ -1,49 +1,49 @@
-function [U, Nit,iterations] = ndim_linesolver(g,maxwidth,e,tol,display)
-%% Description - QCE in 2D
-% Computes the quasi-convex hull of U (in 2D) along grid directions of max
-% width numdir. Sweeps along each each vector using the 1D solver.
+function [QCE,its] = ndim_linesolver(g,width,e,tol)
+%% Description - Quasiconvex envelope in 2D
+%------------------------------------------------------------------------%
+%     Returns the quasiconvex envelope of g using the line solver (see ref 
+%     below). It is assumed that the computational domain is a uniform
+%     partition of [-1,1]^2.
+% 
+%     ref: https://arxiv.org/abs/1612.05584
+%
+%     Parameters
+%     ----------
+%     g     : Obstacle 
+%     width : Width of direction set
+%     e     : e used to generate robustly quasiconvex envelopes (e>0)
+%     tol   : Tolerance used in fixed point iterations
+%
+%     Returns
+%     -------
+%     QCE : (e-Robustly) Quasiconvex envelope
+%     its : Iterations required for convergence to tolerance
+%------------------------------------------------------------------------%
 %% set up
-U = g;
+QCE = g;               %Initialization
 err = inf;
-V = gridvectors(maxwidth);%Returns set of grid directions with max width of numdir.
-Nit = 0;
-iterations = 0;
+V = gridvectors(width);%Returns set of grid directions with max width of width.
+its = 0;
 %% solver
 while err  > tol
     
-    Uold = U;
+    Uold = QCE;
     for i = 1 : size(V,1)
-            [U,its] =  QuasiConvex2D_v2(U,V(i,1),V(i,2),e);
-%             Nit = Nit + its;
-            
+            QCE =  QuasiConvex2D(QCE,V(i,1),V(i,2),e);            
             if i>=3 %unnecessary to sweep on rotated grids for (1,0) and 
                 %(0,1) since they're orthogonal to each other.
-                Ur = rot90(U);
-                [Ur,its] = QuasiConvex2D_v2(Ur,V(i,1),V(i,2),e);
-                U = rot90(Ur,-1);
-%                 Nit = Nit + its;
-            end
-            
-            if display
-                info(maxwidth,err,Nit,V(i,1),V(i,2))
+                Ur = rot90(QCE);
+                Ur = QuasiConvex2D(Ur,V(i,1),V(i,2),e);
+                QCE = rot90(Ur,-1);
             end
     end
-    iterations = iterations + 1;
-    Nit = Nit + 1;
-    err = norm(U(:)-Uold(:),inf);
+    its = its + 1;
+    err = norm(QCE(:)-Uold(:),inf);
 end
 
-if display
-    info(maxwidth,err,Nit,V(i,1),V(i,2))
-end
 if e > 0
    gmin = min(g(:));
-   U = max(U,gmin);
+   QCE = max(QCE,gmin);
 end
 
-end
-
-function info(maxwidth,err,Nit,v1,v2)
-disp(['max width = ', num2str(maxwidth), ',  err = ', num2str(err),...
-    ', #Its = ', num2str(Nit), ' , Direction = (', num2str(v1), ',', num2str(v2),')'] )
 end
